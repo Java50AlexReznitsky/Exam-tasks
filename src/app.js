343fa4a89e8608a0258acda28dfea9f224095b38
Barso2 <reznitskyalex@gmail.com>
2023-02-27 02:04:10 +0200

    HW-29 half_solved

--- src/app.js
+++ src/app.js
@@ -1,62 +1,29 @@
 "use strict";
-// for(let i = 0;i<3;i++){
-//     setTimeout(()=>console.log(i));
-// }
-let num;
-num = 12;
-let str = "abcd";
-str = "lmn";
-let style;
-style = " ";
-let color;
-function f1(n1, n2) {
-    return n1 + n2;
-}
-// f1(1, "2"); // expected error because of f1 n2 is declared as number-type
-function f2(array) {
-    return array.reduce((res, curr) => res + curr);
-}
-function stringNumberAction(strNum) {
-    let res;
-    if (typeof strNum == "string") {
-        res = strNum.length;
+const aChar = 'a'.charCodeAt(0);
+const zChar = 'z'.charCodeAt(0);
+function shiftRound(string, shift) {
+    shift < 0 ? shift = Math.abs(shift) : shift;
+    let checkedSymbol;
+    const strArr = [];
+    for (const char of string) {
+        checkedSymbol = checkSymbol(char);
+        if ((checkedSymbol + shift) > zChar) {
+            checkedSymbol = (aChar + Math.abs(zChar - (shift + checkedSymbol))) - 1;
+            strArr.push(String.fromCharCode(checkedSymbol));
+        }
+        else if (Number.isFinite(checkedSymbol)) {
+            let shiftedCode = checkedSymbol + shift;
+            strArr.push(String.fromCharCode(shiftedCode));
+        }
+        else {
+            strArr.push(char);
+        }
     }
-    else {
-        res = strNum * 2;
-    }
-    return res;
+    return strArr.join("");
 }
-console.log(stringNumberAction(10));
-let ar;
-ar = [1, 2, 3];
-ar = ["1", "g"];
-let ar1;
-ar1 = [1, "a"];
-let someColor;
-let someComparator;
-someComparator = (n1, n2) => n1 - n2;
-someComparator = (f1);
-const person = { id: 123, name: "Vasya" };
-//hw29
-function shiftRound(str, shift = 1) {
-    //TODO
-    //shift must be any positive number
-    //adds shift to ASCII codes of lower case letters
-    //if the shifted code will be greater then ASCII code "z"
-    //the code should be begun from ASCII code "a"
-    //source letter 'a' will be 'c' if "shift" = 2
-    //source letter 'z' will be 'b' if "shift" = 2   
-    //example  shiftRound("aabx!",4) => ("eefb!") 
-    return " ";
+function checkSymbol(symbol) {
+    let symbolCode = symbol.charCodeAt(0);
+    return (symbolCode >= aChar && symbolCode <= zChar) ? symbolCode : NaN;
 }
-function unshiftRound(str, shift = 1) {
-    //shift must be any positive number
-    //subtracts shift to ASCII codes of lower case letters
-    //if the shifted code will be greater then ASCII code "z"
-    //the code should be begun from ASCII code "a"
-    //source letter 'a' will be 'c' if "shift" = 2
-    //source letter 'z' will be 'b' if "shift" = 2   
-    //example  unshiftRound("eefb!",4) => ("aabx!!") 
-    return " ";
-}
+console.log(shiftRound("aabx!", -4));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,28 @@
-// for(let i = 0;i<3;i++){
-//     setTimeout(()=>console.log(i));
-// }
-let num: number;
-num = 12;
-let str: string = "abcd";
-str = "lmn"
-let style: "backgroundColor" | "color" | " ";
-style = " ";
-let color: "red" | "blue" | "green";
-function f1(n1: number, n2: number): number {
-    return n1 + n2
-}
-// f1(1, "2"); // expected error because of f1 n2 is declared as number-type
-function f2(array: number[]): number {
-    return array.reduce((res, curr) => res + curr);
-}
-function stringNumberAction(strNum: string | number): number {
-    let res: number;
-    if (typeof strNum == "string") {
-        res = strNum.length;
-    } else {
-        res = strNum * 2;
+
+const aChar: number = 'a'.charCodeAt(0);
+const zChar: number = 'z'.charCodeAt(0);
+function shiftRound(string: string, shift: number): string {
+    shift < 0 ? shift = Math.abs(shift) : shift;
+    let checkedSymbol: number;
+    const strArr: string[] = [];
+    for (const char of string) {
+        checkedSymbol = checkSymbol(char)
+        if ((checkedSymbol + shift) > zChar) {
+            checkedSymbol = (aChar + Math.abs(zChar - (shift + checkedSymbol))) - 1;
+            strArr.push(String.fromCharCode(checkedSymbol));
+        }
+        else if (Number.isFinite(checkedSymbol)) {
+            let shiftedCode = checkedSymbol + shift;
+            strArr.push(String.fromCharCode(shiftedCode));
+        }
+        else {
+            strArr.push(char)
+        }
     }
-    return res;
+    return strArr.join("");
 }
-console.log(stringNumberAction(10))
-let ar: number[] | string[]
-ar = [1, 2, 3]
-ar = ["1", "g"]
-let ar1: (number | string)[]
-ar1 = [1, "a"];
-type Color = "red" | "blue" | "green" | "grey" | "yellow";
-let someColor: Color;
-type Comparator = (num1: number, num2: number) => number;
-let someComparator: Comparator;
-someComparator = (n1, n2) => n1 - n2;
-someComparator = (f1)
-type Person = {
-    id: number,
-    name: string,
-    age?: number // ? is not must have
+function checkSymbol(symbol: string): number {
+    let symbolCode = symbol.charCodeAt(0);
+    return (symbolCode >= aChar && symbolCode <= zChar) ? symbolCode : NaN;
 }
-const person: Person = { id: 123, name: "Vasya" };
-//hw29
-function shiftRound(str: string, shift: number = 1): string {
-    //TODO
-    //shift must be any positive number
-    //adds shift to ASCII codes of lower case letters
-    //if the shifted code will be greater then ASCII code "z"
-    //the code should be begun from ASCII code "a"
-    //source letter 'a' will be 'c' if "shift" = 2
-    //source letter 'z' will be 'b' if "shift" = 2   
-    //example  shiftRound("aabx!",4) => ("eefb!") 
-    return " "
-}
-function unshiftRound(str: string, shift: number = 1): string {
-    //shift must be any positive number
-    //subtracts shift to ASCII codes of lower case letters
-    //if the shifted code will be greater then ASCII code "z"
-    //the code should be begun from ASCII code "a"
-    //source letter 'a' will be 'c' if "shift" = 2
-    //source letter 'z' will be 'b' if "shift" = 2   
-    //example  unshiftRound("eefb!",4) => ("aabx!!") 
-    return " "
-}
+console.log(shiftRound("aabx!", -4));

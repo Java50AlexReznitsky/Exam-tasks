f279899fedb68a555efa72aaca685c312d96d76b
Barso2 <reznitskyalex@gmail.com>
2023-02-28 20:46:02 +0200

    CW-30

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,6 @@ import { Container } from "./Container";
 const rectangle: Rectangle = new Rectangle(3, 4);
 const square = new Square(10);
 const container:Container = new Container([rectangle,square])
-displaySquarePerimeter(container);
+displaySquarePerimeter(container)
 
 
